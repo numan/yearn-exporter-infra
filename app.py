8d1c76fd3bf9e342cc2ac14c6a5c7cfe31e551dd
numan <numan856@gmail.com>
2022-03-01 12:57:46 -0800

    Switch to an EC2 instance running all our scheduled jobs. Disk I/O on EFS was just awful

--- app.py
+++ app.py
@@ -3,21 +3,22 @@ import json
 import os
 
 import aws_cdk as cdk
-from aws_cdk import Duration, RemovalPolicy
+from aws_cdk import Duration, RemovalPolicy, Size
 from aws_cdk import aws_applicationautoscaling as app_autoscaling
+from aws_cdk import aws_autoscaling as autoscaling
 from aws_cdk import aws_cloudfront as cloudfront
 from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_ecr as ecr
 from aws_cdk import aws_ecs as ecs
-from aws_cdk import aws_efs as efs
 from aws_cdk import aws_iam as iam
 from aws_cdk import aws_logs as logs
 from aws_cdk import aws_s3 as s3
 from aws_cdk import aws_secretsmanager as secretsmanager
 from constructs import Construct
 
-from yearn_apy_exporter_infra.yearn_apy_exporter_infra_stack import \
-    YearnApyExporterInfraStack
+from yearn_apy_exporter_infra.yearn_apy_exporter_infra_stack import (
+    YearnApyExporterInfraStack,
+)
 
 
 class YearnExporterInfraApp(cdk.Stack):
@@ -47,6 +48,8 @@ class YearnExporterInfraApp(cdk.Stack):
                         "MAINNET_ETHERSCAN_TOKEN": "",
                         "FANTOM_WEB3_PROVIDER": "",
                         "FTMSCAN_TOKEN": "",
+                        "GRAFANA_URL": "",
+                        "GRAFANA_API_KEY": "",
                     }
                 ),
                 generate_string_key="_",  # Needed just to we can provision secrets manager with a template. Not used.
@@ -81,6 +84,7 @@ class YearnExporterInfraApp(cdk.Stack):
                     allowed_origins=["*"],
                 )
             ],
+            public_read_access=True,
         )
 
         cloudfront.CloudFrontWebDistribution(
@@ -101,14 +105,36 @@ class YearnExporterInfraApp(cdk.Stack):
         bucket.grant_read(iam_user)
         repository.grant_pull_push(iam_user)
 
+        cluster = ecs.Cluster(self, "ApyExporterCluster", vpc=vpc)
+
+        asg_provider = ecs.AsgCapacityProvider(
+            self,
+            "ApyExporterAsgCapacityProvider",
+            auto_scaling_group=autoscaling.AutoScalingGroup(
+                self,
+                "ApyExporterAsg",
+                instance_type=ec2.InstanceType("m6a.4xlarge"),
+                vpc=vpc,
+                machine_image=ecs.EcsOptimizedImage.amazon_linux2(),
+            ),
+        )
+
+        cluster.add_asg_capacity_provider(asg_provider)
+
+        asg_provider.auto_scaling_group.role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name(
+                "AmazonSSMManagedInstanceCore"
+            )
+        )
+
         YearnApyExporterInfraStack(
             self,
             "YearnMainnetApyExporterInfraStack",
-            vpc=vpc,
             log_group=apy_log_group,
             repository=repository,
             bucket=bucket,
             network="mainnet",
+            cluster=cluster,
             container_secrets={
                 "WEB3_PROVIDER": ecs.Secret.from_secrets_manager(
                     secrets, "MAINNET_WEB3_PROVIDER"
@@ -116,10 +142,15 @@ class YearnExporterInfraApp(cdk.Stack):
                 "ETHERSCAN_TOKEN": ecs.Secret.from_secrets_manager(
                     secrets, "MAINNET_ETHERSCAN_TOKEN"
                 ),
+                "GRAFANA_URL": ecs.Secret.from_secrets_manager(secrets, "GRAFANA_URL"),
+                "GRAFANA_API_KEY": ecs.Secret.from_secrets_manager(
+                    secrets, "GRAFANA_API_KEY"
+                ),
             },
             explorer_url="https://api.etherscan.io/api",
             schedule=app_autoscaling.Schedule.cron(
-                hour="2",
+                hour="7",
+                minute="0",
             ),
             **kwargs
         )
@@ -127,11 +158,11 @@ class YearnExporterInfraApp(cdk.Stack):
         YearnApyExporterInfraStack(
             self,
             "YearnFantomApyExporterInfraStack",
-            vpc=vpc,
             log_group=apy_log_group,
             repository=repository,
             bucket=bucket,
             network="ftm-main",
+            cluster=cluster,
             container_secrets={
                 "WEB3_PROVIDER": ecs.Secret.from_secrets_manager(
                     secrets, "FANTOM_WEB3_PROVIDER"
@@ -139,10 +170,15 @@ class YearnExporterInfraApp(cdk.Stack):
                 "FTMSCAN_TOKEN": ecs.Secret.from_secrets_manager(
                     secrets, "FTMSCAN_TOKEN"
                 ),
+                "GRAFANA_URL": ecs.Secret.from_secrets_manager(secrets, "GRAFANA_URL"),
+                "GRAFANA_API_KEY": ecs.Secret.from_secrets_manager(
+                    secrets, "GRAFANA_API_KEY"
+                ),
             },
             explorer_url="https://api.ftmscan.com/api",
             schedule=app_autoscaling.Schedule.cron(
-                hour="2",
+                hour="7",
+                minute="0",
             ),
             **kwargs
         )
--- yearn_apy_exporter_infra/yearn_apy_exporter_infra_stack.py
+++ yearn_apy_exporter_infra/yearn_apy_exporter_infra_stack.py
@@ -1,69 +1,47 @@
 from tokenize import Number
-from typing import Mapping
-from aws_cdk import RemovalPolicy, Stack
+from typing import Mapping, Optional
+
+from aws_cdk import RemovalPolicy, Size, Stack
 from aws_cdk import aws_applicationautoscaling as app_autoscaling
 from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_ecr as ecr
 from aws_cdk import aws_ecs as ecs
 from aws_cdk import aws_ecs_patterns as ecs_patterns
+from aws_cdk import aws_efs as efs
 from aws_cdk import aws_logs as logs
 from aws_cdk import aws_s3 as s3
 from aws_cdk import aws_secretsmanager as secretsmanager
-from aws_cdk import aws_efs as efs
 from constructs import Construct
+
 
 class YearnApyExporterInfraStack(Stack):
     def __init__(
         self,
         scope: Construct,
         construct_id: str,
-        vpc: ec2.IVpc,
         log_group: logs.LogGroup,
         repository: ecr.IRepository,
         bucket: s3.IBucket,
         container_secrets: Mapping[str, secretsmanager.Secret],
         schedule: app_autoscaling.Schedule,
-        explorer_url:str,
+        cluster: ecs.ICluster,
+        explorer_url: str,
         network: str,
-        cpu: Number = 4096,
-        memory: Number = 10240,
         **kwargs
     ) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        cluster = ecs.Cluster(self, "ApyExporterCluster", vpc=vpc)
 
-
-        sg = ec2.SecurityGroup(
-            self, "EFSSecurityGroup", vpc=vpc, allow_all_outbound=True
-        )
-        sg.connections.allow_from(sg, ec2.Port.all_traffic())
-
-        apy_cache_fs = efs.FileSystem(
-            self,
-            "ApyExporterCacheFileSystem",
-            vpc=vpc,
-            lifecycle_policy=efs.LifecyclePolicy.AFTER_14_DAYS,
-            performance_mode=efs.PerformanceMode.GENERAL_PURPOSE,
-            security_group=sg,
-            removal_policy=RemovalPolicy.DESTROY,
-        )
-
-        task_definition = ecs.FargateTaskDefinition(
+        task_definition = ecs.Ec2TaskDefinition(
             self,
             "ApyExporterTaskDefinition",
-            cpu=cpu,
-            memory_limit_mib=memory,
             volumes=[
                 ecs.Volume(
                     name="brownie",
-                    efs_volume_configuration=ecs.EfsVolumeConfiguration(
-                        file_system_id=apy_cache_fs.file_system_id,
-                    ),
+                    host=ecs.Host(source_path=f"/data/{network.lower()}"),
                 )
             ],
         )
-
 
         container = task_definition.add_container(
             "s3-apy",
@@ -77,8 +55,7 @@ class YearnApyExporterInfraStack(Stack):
                 stream_prefix="apy",
                 mode=ecs.AwsLogDriverMode.NON_BLOCKING,
             ),
-            cpu=cpu,
-            memory_reservation_mib=memory,
+            memory_reservation_mib=1024,
             environment={
                 "AWS_BUCKET": bucket.bucket_name,
                 "EXPLORER": explorer_url,
@@ -95,21 +72,14 @@ class YearnApyExporterInfraStack(Stack):
             )
         )
 
-        scheduled_task = ecs_patterns.ScheduledFargateTask(
+        scheduled_task = ecs_patterns.ScheduledEc2Task(
             self,
             "ApyExporterTask",
             cluster=cluster,
-            scheduled_fargate_task_definition_options=ecs_patterns.ScheduledFargateTaskDefinitionOptions(
+            scheduled_ec2_task_definition_options=ecs_patterns.ScheduledEc2TaskDefinitionOptions(
                 task_definition=task_definition,
             ),
             schedule=schedule,
-
         )
 
-
-
-        for task_sg in scheduled_task.task.security_groups:
-            task_sg.connections.allow_to(apy_cache_fs.connections, ec2.Port.all_traffic())
-
         bucket.grant_read_write(scheduled_task.task_definition.task_role)
-
